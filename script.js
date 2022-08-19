7201847cceab341ac0b36a9cd9994096e01e3720
Abigail <neneojingwa@gmail.com>
2022-08-19 13:17:07 +0100

    modified the file

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ let score =  divide(25,5)
 console.log(answer)
 console.log(total)
 console.log(score)
+
+// a function that loop through an array and return true or false 
+
+function nigerian(languages){
+    for (let lang of languages) {
+        if (lang =='igbo' || lang =='yoruba' || lang =='Hausa') {
+            return true
+        }
+        
+    }
+    return false
+
+}
+nigerian(['akwa-ibom','item','ebonyi','igbo'])
