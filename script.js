5ad4aaf38a7446965aae191ad260c77e8aa5d606
Abigail <neneojingwa@gmail.com>
2022-08-19 08:30:08 +0100

    a function to add, mutiply and divide

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ function numOfTimes(times){
         rollDice()
     }
 }
-numOfTimes(30)
+// numOfTimes(30)
+// code to mutiply, add and divide parameter in a function
+function mutiply(x,y){
+    console.log(x * y)
+}
+mutiply(5,5)
+function add(a,b){
+    console.log(a + b)
+}
+add(20,20)
+function divide(j,k){
+    console.log(j / k)
+}
+divide(25,5)
