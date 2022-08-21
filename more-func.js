7e4e0a07279b0917a2a21da36898a79e87b05b14
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-08-21 05:08:41 +0100

    adding a func in  an object

--- more-func.js
+++ more-func.js
@@ -11,9 +11,20 @@ const mutiply = function(x,y){
 const divide = function(x,y){
     return  x / y
 }
-const basicOpreator = [add,subtract,mutiply,divide]
-for (let opreator of basicOpreator) {
-   let answer = opreator(50,2)
-   console.log(answer)
+// const basicOpreator = [add,subtract,mutiply,divide]
+// for (let opreator of basicOpreator) {
+//    let answer = opreator(50,2)
+//    console.log(answer)
     
+// }
+// as an object 
+let opreators = {
+    add : add,
+    mutiply: mutiply,
+    divide : divide
+}
+for (let func in opreators) {
+    let result = opreators[func]
+    console.log(result(4,5))
+
 }
